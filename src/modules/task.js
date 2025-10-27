658c4eea7b8dffabd33eaf331d003c2e0ff82f0a
Austin <ausbratton@gmail.com>
2025-10-27 06:12:55 -0500

    Change handling of Task class priority parameter

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,9 +1,11 @@
+const priorityLevels = ["low", "medium", "high"];
+
 export class Task {
   constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
-    this.priority = ["!", "!!", "!!!"];
+    this.priority = priorityLevels[priority];
     this.status = "Incomplete";
   }
 }
